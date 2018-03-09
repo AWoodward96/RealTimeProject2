5cb9dd83ef123866ce28ab6e9884783f460a38ef
AWoodward <akw7657@g.rit.edu>
2018-03-09 17:11:56 -0500

    space now jumps as well as w

--- client/main.js
+++ client/main.js
@@ -36,7 +36,7 @@ const keyDownHandler = (e) => {
     var keyPressed = e.which;
 
     // W OR UP
-    if (keyPressed === 87 || keyPressed === 38) {
+    if (keyPressed === 87 || keyPressed === 38 || keyPressed===32) {
         moveUp = true;
     }
     // A OR LEFT
@@ -70,7 +70,7 @@ const keyUpHandler = (e) => {
     var keyPressed = e.which;
 
     // W OR UP
-    if (keyPressed === 87 || keyPressed === 38) {
+    if (keyPressed === 87 || keyPressed === 38 || keyPressed===32) {
         moveUp = false;
     }
     // A OR LEFT
